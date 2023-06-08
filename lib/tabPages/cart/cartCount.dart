2b7907a4af9c65a1668205df928e75fafe810876
Farah Abu Zalam <abuzalamfarah2002@gmail.com>
2023-06-08 09:47:00 +0400

    new5

--- lib/tabPages/cart/cartCount.dart
+++ lib/tabPages/cart/cartCount.dart
@@ -1,11 +1,15 @@
-
 import 'package:flutter/material.dart';
+import 'package:testproject/constants/appcolor.dart';
+import 'package:testproject/constants/appsize.dart';
+import 'package:testproject/globalVariables.dart';
 import 'package:testproject/modules/cartListModule.dart';
+
 
 import '../../modules/itemContainerModule.dart';
 class CartCount extends StatefulWidget {
 
   ItemContainerModule item;
+  Function()? refresh;
 
    CartCount({Key? key, required this.item}) : super(key: key);
 
@@ -21,9 +25,115 @@ class _CartCountState extends State<CartCount> {
     return Container(
       child: Row(
         children: [
+          Image.network(
+            widget.item.image,
+            fit: BoxFit.cover,
+            height: AppSize.itemImageHight,
+          ),
+          Column(
+              children: [
+                Text(
+                  '${widget.item.name},\n ${widget.item.price}',
+                  style: TextStyle(color: AppColor.black, fontSize: 15),
+                )
+              ]
 
+          ),
+          Column(
+            children: [ Row(
+              children: [
+                widget.item.count == null
+                    ? SizedBox()
+                    : _icon(Icons.remove, () => _minimise()),
+                widget.item.count == null
+                    ? SizedBox()
+                    : Text(
+                  widget.item.count.toString(),
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: AppColor.red,
+                      backgroundColor:
+                      AppColor.white.withOpacity(0.3)),
+                ),
+                _icon(Icons.add, () => _add()),
+
+                Text(
+                  '${widget.item.price}',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                      color: AppColor.black,
+                      backgroundColor: Colors.grey,
+                      fontSize: 20),
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete_outlined),
+                  onPressed: ()=> GlobalVariables.cartList.removeWhere((element) => element.item == widget.item),
+                ),
+              ],
+            ),
+            ],
+          ),
         ],
       ),
     );
+  }
+
+  _press(int num) {
+    setState(() {
+      GlobalVariables.selectedTap = num;
+    });
+  }
+  _icon(icon, Function() press) {
+    return Container(
+      height: 40,
+      width: 40,
+      margin: EdgeInsets.symmetric(horizontal: AppSize.margin20),
+      decoration:
+      BoxDecoration(shape: BoxShape.circle, color: AppColor.mainColor),
+      child: IconButton(
+        onPressed: press,
+        icon: Icon(
+          icon,
+          color: AppColor.white,
+          size: 25,
+        ),
+      ),
+    );
+  }
+
+  _add() {
+    setState(() {
+      widget.item.count == null
+          ? widget.item.count = 1
+          : widget.item.count = (widget.item!.count! + 1);
+      print(widget.item.count.toString());
+
+      bool update = false;
+      for (var c in GlobalVariables.cartList) {
+        if (c.item == widget.item)
+          update = true;
+      }
+      if (!update)
+        GlobalVariables.cartList.add(CartListModule(item: widget.item));
+
+    });
+    widget.refresh!();
+  }
+
+  _minimise() {
+    setState(() {
+      widget.item.count == 1
+          ? widget.item.count = null
+          : widget.item.count = (widget.item.count! -1);
+      //print(widget.item.count.toString());
+
+      bool update = false;
+
+      if(widget.item.count == null) {
+        GlobalVariables.cartList.removeWhere((element) => element.item == widget.item);
+        print(GlobalVariables.cartList.length.toString());
+        widget.refresh!();
+      }
+    });
   }
 }
--- lib/tabPages/cart/cartPage.dart
+++ lib/tabPages/cart/cartPage.dart
@@ -1,21 +1,11 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:testproject/constants/appcolor.dart';
-import 'package:testproject/navigateItem.dart';
-import 'package:testproject/sharedWidgets/itemContainer.dart';
 import 'package:testproject/tabPages/cart/cartCount.dart';
-
-import '../../constants/appsize.dart';
 import '../../globalVariables.dart';
-import '../../modules/cartListModule.dart';
-import '../../modules/itemContainerModule.dart';
+
 
 class CartPage extends StatefulWidget {
 
-  ItemContainerModule item;
-  Function()? refresh;
-
-   CartPage({Key? key,required this.item}) : super(key: key);
+   CartPage({Key? key,}) : super(key: key);
 
   @override
   State<CartPage> createState() => _CartPageState();
@@ -26,121 +16,12 @@ class CartPage extends StatefulWidget {
 
   @override
   Widget build(BuildContext context) {
-    //print(GlobalVariables.cartList[0].item);
-    GlobalVariables.cartList.map((e) => CartCount(item: e.item)).toList();
-
-    return Expanded(child:  Container (
-          child: Row(
-            children: [
-              Image.network(
-                widget.item.image,
-                fit: BoxFit.fill,
-                height: AppSize.itemImageHight,
-              ),
-              Column(
-                children: [
-                  Text(
-                    '${widget.item.name},\n ${widget.item.price}',
-                    style: TextStyle(color: AppColor.black, fontSize: 20),
-                  )
-                ]
-
-              ),
-              Column(
-                children: [ Row(
-                  children:[
-                  widget.item.count == null
-                      ? SizedBox()
-                      : _icon(Icons.remove, () => _minimise()),
-                  widget.item.count == null
-                      ? SizedBox()
-                      : Text(
-                    widget.item.count.toString(),
-                    style: TextStyle(
-                        fontSize: 25,
-                        color: AppColor.red,
-                        backgroundColor:
-                        AppColor.white.withOpacity(0.3)),
-                  ),
-                  _icon(Icons.add, () => _add()),
-
-                    Text(
-                      '${widget.item.price}',
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                          color: AppColor.black,
-                          backgroundColor: Colors.grey,
-                          fontSize: 20),
-                    ),
-                    NavigateItem( navigateItem: 'trash'.tr(),
-                        navigateIcon: Icon(Icons.delete_outline),
-                        pageNumber: 2,
-                        press: () => _press(2),),
-                  ],
-                ),
-                ],
-              ),
-            ],
-          ),
-    ),
+    return Expanded(
+        child: ListView(
+      children:
+      GlobalVariables.cartList.map((e) => CartCount(item: e.item)).toList()
+    )
     );
-   }
-  _press(int num) {
-    setState(() {
-      GlobalVariables.selectedTap = num;
-    });
-  }
-  _icon(icon, Function() press) {
-    return Container(
-      height: 40,
-      width: 40,
-      margin: EdgeInsets.symmetric(horizontal: AppSize.margin20),
-      decoration:
-      BoxDecoration(shape: BoxShape.circle, color: AppColor.mainColor),
-      child: IconButton(
-        onPressed: press,
-        icon: Icon(
-          icon,
-          color: AppColor.white,
-          size: 25,
-        ),
-      ),
-    );
-  }
 
-  _add() {
-    setState(() {
-      widget.item.count == null
-          ? widget.item.count = 1
-          : widget.item.count = (widget.item!.count! + 1);
-      print(widget.item.count.toString());
-
-      bool update = false;
-      for (var c in GlobalVariables.cartList) {
-        if (c.item == widget.item)
-          update = true;
-      }
-      if (!update)
-        GlobalVariables.cartList.add(CartListModule(item: widget.item));
-
-    });
-    widget.refresh!();
-  }
-
-  _minimise() {
-    setState(() {
-      widget.item.count == 1
-          ? widget.item.count = null
-          : widget.item.count = (widget.item.count! -1);
-      //print(widget.item.count.toString());
-
-      bool update = false;
-
-      if(widget.item.count == null) {
-        GlobalVariables.cartList.removeWhere((element) => element.item == widget.item);
-        print(GlobalVariables.cartList.length.toString());
-        widget.refresh!();
-      }
-    });
   }
  }
